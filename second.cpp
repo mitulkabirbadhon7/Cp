fba81b4702a99caa58e06dc16b1921f684bb2704
Badhon <mitulkabirbadhon7@gmail.com>
2026-03-16 05:51:21 +0600

    Final push check

---
+++ second.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x = 2;
+    int y = 5;
+    cout << x + y <<endl;
+
+    return 0;
+}
